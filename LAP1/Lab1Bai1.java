4468015ecd3dc43fd701ff081bb5cdb80fc5f6c4
thunguyenngocanh <121885372+ngocanhthu@users.noreply.github.com>
2024-02-29 23:05:51 +0700

    Add files via upload

---
+++ LAP1/Lab1Bai1.java
@@ -0,0 +1,17 @@
+package LAP1;
+
+import java.util.Scanner;
+
+/**
+ * Nguyễn Ngọc Anh Thư
+ */
+public class Lab1Bai1{
+public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Họ và tên: ");
+    String hoTen = scanner.nextLine();
+    System.out.print("Điểm TB: ");
+    double diemTB = scanner.nextDouble();
+    System.out.printf("%s %f điểm", hoTen, diemTB);
+    }
+}
